e3c16891a5661bdb43f629ef574ae4246beed905
hakohumi <mihumihu32@gmail.com>
2021-12-03 13:01:49 +0900

    wip インターフェースの追加

---
+++ translator.py
@@ -0,0 +1,9 @@
+# TODO: 翻訳API インターフェース
+# どのAPIでも共通して使用できるように、抽象化する
+
+def translate(japanese_strings: str) -> str:
+    """
+    translate 英文を日本語に翻訳する
+    """
+    output_string: str = ""
+    return output_string
