3f9888c7e34a265d333a2aae9e0965ecf975f594
hakohumi <mihumihu32@gmail.com>
2021-12-04 22:59:40 +0900

    Translateインターフェースとして変更

--- translator.py
+++ translator.py
@@ -1,9 +1,15 @@
 # TODO: 翻訳API インターフェース
 # どのAPIでも共通して使用できるように、抽象化する
 
-def translate(japanese_strings: str) -> str:
-    """
-    translate 英文を日本語に翻訳する
-    """
-    output_string: str = ""
-    return output_string
+from abc import ABCMeta, abstractmethod
+
+
+class Translate(metaclass=ABCMeta):
+
+    @abstractmethod
+    def translate(self, src_japanese: str) -> str:
+        """
+        translate 英文を日本語に翻訳する
+        """
+        print("interface")
+        return ""
